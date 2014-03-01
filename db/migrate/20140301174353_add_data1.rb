952faebf53a73a2e1c00ad3a4e53b890265f6c11
chronicqazxc <chronicqazxc@gmail.com>
2014-03-02 02:15:09 +0800

    Add Data

---
+++ db/migrate/20140301174353_add_data1.rb
@@ -0,0 +1,54 @@
+class AddData1 < ActiveRecord::Migration
+  def change
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區光復北路66號", :name => "博仁綜合醫院")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區八德路三段123號", :name => "摩斯漢堡八德店")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區八德路三段251號", :name => "統一星巴克德復門市")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區南京東路四段156號", :name => "首都大飯店")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區八德路三段178號", :name => "臺北市稅捐稽徵處松山分處")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區八德路三段168號", :name => "台灣麥當勞八德店")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區八德路三段10之1號", :name => "統一星巴克台視門市")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區八德路三段112號", :name => "拿坡里八德店")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區新中街7號", :name => "東社市場")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區市民大道四段", :name => "市民大道延吉段地下停車場")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區八德路三段99巷17弄13號", :name => "松山1號")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區敦化北路1號", :name => "臺北市松山區運動中心")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區敦化北路3號", :name => "臺北田徑場")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區八德路三段25號", :name => "臺北市立社會教育館")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區寧安街12號", :name => "臺北市私立育達高級商業家事職業學校")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區南京東路四段56號", :name => "統一星巴克北寧門市")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區復興南路一段39號", :name => "微風廣場")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區長安東路二段229號8樓", :name => "臺北市立圖書館中崙分館")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區敦化北路2號", :name => "臺北市松山區敦化國民小學")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "南京東路三段300號", :name => "臺北市立敦化國民中學")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區敦化北路100號B1", :name => "IKEA宜家家居")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區南京東路三段337號", :name => "微風南京")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區長安東路二段219號", :name => "總督大戲院")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區慶城街8之1號", :name => "麥當勞慶城店")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "松山區", :address => "臺北市松山區八德路二段449號1樓", :name => "漢堡王八德分公司")
+    @crapper.save
+  end
+end
