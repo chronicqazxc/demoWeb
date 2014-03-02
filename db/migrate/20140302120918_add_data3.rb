8ad93daee4c53acece3a8bb0879d68e969aa982f
chronicqazxc <chronicqazxc@gmail.com>
2014-03-02 20:53:41 +0800

    add data3

---
+++ db/migrate/20140302120918_add_data3.rb
@@ -0,0 +1,222 @@
+class AddData3 < ActiveRecord::Migration
+  def change
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區臥龍街427巷51弄底29號", :name => "信義24號")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區嘉興街301號", :name => "臺北市政府警察局六張犁派出所")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區基隆路二段99-1號", :name => "台灣麥當勞台北三興分公司")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區吳興街284巷17號之1", :name => "松山寺")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區吳興街250號", :name => "臺北醫學大學醫綜16樓")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區吳興街262號", :name => "臺北市政府警察局吳興街派出所")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區吳興街252號", :name => "臺北醫學大學附設醫院")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區信安街103巷201弄10號", :name => "信義15號")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區吳興街156巷6號", :name => "臺北市立圖書館三興分館")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區基隆路二段99號", :name => "臺北市信義區三興國民小學")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區莊敬路182號", :name => "丹堤咖啡莊敬店")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區基隆路二段7號", :name => "星期五餐廳世貿店")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區吳興街583巷1弄28號", :name => "信義1號")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區吳興街583巷6弄35號", :name => "信義17號")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區信義路五段150巷56之34號", :name => "信義23號")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松德路309號", :name => "臺北市立聯合醫院松德院區")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區信義路五段150巷", :name => "中強公園")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松勤街100號", :name => "臺北市信義區運動中心")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松仁路226號", :name => "臺北市信義區吳興國民小學")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區莊敬路423巷8弄1號", :name => "臺北市立信義國民中學")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區莊敬路391巷11號", :name => "松山地政事務所")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區莊敬路469號", :name => "摩斯漢堡吳興街莊敬")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區吳興街243號", :name => "摩斯漢堡吳興街分公司")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松平路81號", :name => "信義三張里地下停車場")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區莊敬路391巷11弄2號", :name => "臺北市災害應變中心地下停車場")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松勤街60號", :name => "臺北市信義區信義國民小學")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區莊敬路273號", :name => "爭鮮迴轉壽司北醫店")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區福德街198號", :name => "臺北市政府警察局福德街派出所")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區忠孝東路五段790巷27號", :name => "臺北市私立祐德高級中學")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區福德街251巷", :name => "信義公園5號")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區福德街251巷33號", :name => "松山慈惠堂")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區福德街253號", :name => "臺北市信義區福德國民小學")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區福德街221巷15號", :name => "臺北市立塯公國民中學活動中心")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區福德街221巷12號", :name => "松山奉天宮")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區忠孝東路五段538號", :name => "丹堤咖啡忠孝松山店")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區忠孝東路五段522號", :name => "台灣麥當勞台北忠孝4分公司")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區忠孝東路五段666號", :name => "春光公園地下停車場")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松山路664-10號", :name => "信義2號")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松山路655號", :name => "臺北市立松山高級家事商業職業學校")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松山路654號", :name => "臺北市立永春高級中學")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區忠孝東路五段439號", :name => "丹提咖啡永春店")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區忠孝東路五段289號1樓", :name => "台灣麥當勞台北忠孝東路第2分公司")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區忠孝東路五段297號1樓", :name => "統一星巴克永春門市")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區忠孝東路五段297號", :name => "遠東愛買忠孝店")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區忠孝東路五段455號", :name => "捷運永春站")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區虎林街104巷185號", :name => "信義13號")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區虎林街104巷133號", :name => "信義12號")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區永吉路468巷36弄15之1號", :name => "信義10號")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松山路294號", :name => "臺北市立圖書館永春分館")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松山路287巷5號", :name => "臺北市信義區永吉國民小學")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區忠孝東路五段629號", :name => "摩斯漢堡松山店")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區虎林街89巷1號", :name => "永春市場")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松山路184巷1號", :name => "信義8號")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區虎林街26巷5號", :name => "財團法人台北市靈源寺")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松隆路161號", :name => "臺北市立永吉國民中學")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區永吉路333號", :name => "臺北市政府警察局五分埔派出所")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松山路181巷1號", :name => "信義7號")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市松山區松山路11號", :name => "松山火車站地下停車場")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區中坡北路57號", :name => "中坡公園地下停車場")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區基隆路一段83巷9號", :name => "臺北市信義區興雅國民小學")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區永吉路278巷1弄30號", :name => "永吉市場")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松隆路123巷7號", :name => "雅祥公園地下停車場")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區永吉路180巷41弄10號", :name => "信義6號")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區永吉路180巷54弄20號", :name => "信義14號")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松仁路95巷20號", :name => "臺北市信義區博愛國民小學")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松德路168巷15號", :name => "臺北市立興雅國民中學")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松仁路95巷20號", :name => "信義安康公園")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松德路200巷18號", :name => "興雅國中地下停車場")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區忠孝東路五段236巷15號", :name => "臺北市立松山高級工農職業學校")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松山路460號", :name => "爭鮮股份有限公司永春分公司")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松山路480號", :name => "中油松山路站")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區基隆路一段49號", :name => "信義5號")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區忠孝東路五段8號", :name => "統一阪急百貨")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松仁路28號", :name => "BELLAVITA百貨")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松高路11號", :name => "誠品信義旗艦店")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松高路19號", :name => "新光三越A4館")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區忠孝東路五段155號1樓", :name => "吉野家市府站")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區忠孝東路五段10號", :name => "時代國際飯店")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區忠孝東路五段2號", :name => "捷運市政府站")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區忠孝東路五段69號", :name => "中油忠孝東路站")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區忠孝東路五段6號", :name => "市政府轉運站")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松仁路2號", :name => "忠信地下停車場")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "台北市信義區光復南路133號", :name => "松山文創園區1號倉庫")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "台北市信義區基隆路一段156號", :name => "臺北市立松山高級中學")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區菸廠路88號", :name => "誠品生活松菸店")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區東興路45號B1", :name => "家樂福東興店")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區基隆路一段156號", :name => "松山高中地下停車場")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區光復南路473巷13弄43號", :name => "中興公園")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區仁愛路四段505號", :name => "國父紀念館")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區光復南路271號", :name => "臺北市信義區光復國民小學")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區忠孝東路四段400號", :name => "捷運國父紀念館站")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區光復南路419巷146號", :name => "光復市場")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區信義路五段5號", :name => "臺北世界貿易展覽中心")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松壽路6號", :name => "臺北世界貿易中心第三館")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區信義路五段15號4樓", :name => "國稅局信義稽徵所")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區信義路5段17號", :name => "臺北市政府警察局信義分局")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區市府路1號", :name => "市府行政中心")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區信義路五段15號2樓", :name => "信義區戶政事務所")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區市府路45號", :name => "台北101購物中心")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松壽路12號", :name => "ATT 4 FUN")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松壽路9號", :name => "新光三越A9館")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松高路12號", :name => "新光三越百貨A8館")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松壽路11號", :name => "新光三越百貨(信義店)")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松壽路18號", :name => "華納威秀電影城")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松仁路38號", :name => "台北寒舍艾美酒店")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松壽路2號", :name => "君悅飯店")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區信義路五段7號", :name => "台北101觀景台91F")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區信義路五段11", :name => "信義廣場地下停車場")
+    @crapper.save
+    @crapper = Crapper.new(:city => "臺北市", :division => "信義區", :address => "臺北市信義區松壽路1號", :name => "市府府前停車場")
+    @crapper.save
+  end
+end
